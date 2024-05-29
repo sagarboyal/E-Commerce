70a273a543867550144d1c9a69a53bd85002591d
Sagar Boyal <sboyal12@gmail.com>
2024-05-29 20:40:54 +0530

    bootstrap and jquery dependency added and admin home page created

--- src/main/java/com/ecommerce/main/controller/MainController.java
+++ src/main/java/com/ecommerce/main/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.ecommerce.main.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
     @GetMapping("/")
     public String getHome(){
-        return "Home Page";
+        return "homepage";
     }
 }
