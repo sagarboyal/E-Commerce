2cfbe4d1c4672de741f8aca592b362896464a7bc
Sagar Boyal <sboyal12@gmail.com>
2024-06-02 21:48:03 +0530

    navbar implemented

--- src/main/java/com/ecommerce/main/controller/MainController.java
+++ src/main/java/com/ecommerce/main/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String getHome(){
-        return "homepage";
+        return "admin/homepage";
     }
 }
