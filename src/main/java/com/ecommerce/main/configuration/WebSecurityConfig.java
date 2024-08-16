2a519a938f141a700e61dc4ca7344c2ea7cdb991
sagarboyal <sboyal12@gmail.com>
2024-08-16 11:45:20 +0530

    Admin Login Page implemented and handler added!!!

--- src/main/java/com/ecommerce/main/configuration/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/main/configuration/WebSecurityConfig.java
@@ -10,13 +10,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
     @Bean
-    SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception{
-        http.
-                authorizeHttpRequests(auth ->
-                        auth.anyRequest().permitAll()
-                        );
+    SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/images/**", "/js/**", "/webjars/**").permitAll() // Allow access to static resources images
+                                .anyRequest().authenticated() // All other requests require authentication
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Custom login page
+                        .permitAll() // Allow access to the login page without authentication
+                );
         return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/com/ecommerce/main/controller/MainController.java
+++ src/main/java/com/ecommerce/main/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,9 @@ public class MainController {
     @GetMapping("/")
     public String getHome(){
         return "admin/homepage";
+    }
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return"admin/login";
     }
 }
